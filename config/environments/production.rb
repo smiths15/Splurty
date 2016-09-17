56ead389d11fb06ed1ea89ca7ed3c3ce48592215
Smith Saintil <smith.saintil@gmail.com>
2016-09-17 16:53:29 +0000

    Adjust production.rb so it comiles assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # Changed false to true to make our application look good
+  config.assets.compile = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
